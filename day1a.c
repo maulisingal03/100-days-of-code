4d1c149a8113f43b4f238f36cbafba20ae62f01c
maulisingal03 <mauliamb03@gmail.com>
2025-11-09 22:14:55 +0530

    Create day1a.c

---
+++ day1a.c
@@ -0,0 +1,14 @@
+//Q1: Write a program to input two numbers and display their sum.
+
+#include<stdio.h>
+int main()
+{
+    int a,b, sum;
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("Sum = %d\n", sum);
+    return 0;
+}
