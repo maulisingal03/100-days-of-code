e0d13d71c160ce6506bae0ca9c5617cc11c66e73
maulisingal03 <mauliamb03@gmail.com>
2025-11-09 23:13:31 +0530

    Create day8ques16.c

---
+++ day8ques16.c
@@ -0,0 +1,28 @@
+//Q16: Write a program to input three numbers and find the largest among them using if–else.
+#include<stdio.h>
+int main()
+{
+    int first;
+    printf("Enter the First Number %d",first);
+    scanf("%d",&first);
+    
+    int second;
+    printf("Enter the Second Number %d",second);
+    scanf("%d",&second);
+    
+    int third;
+    printf("Enter the Third Number %d",third);
+    scanf("%d",&third);
+    
+    if (first>second && first>third)
+        {printf("The Greatest Number is %d", first);}
+        
+    else if (second>first && second>third)
+        {printf("The Greatest Number is %d", second);}
+        
+    else if (third>second && third>first)
+        {printf("The Greatest Number is %d", third);}
+        
+    
+    return 0;
+}
