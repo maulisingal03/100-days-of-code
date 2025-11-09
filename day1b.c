ae2c381db7cc1a5717a0dbfde3797d72302406a7
maulisingal03 <mauliamb03@gmail.com>
2025-11-09 22:15:32 +0530

    Implement basic arithmetic operations for two numbers
    Fix input format specifiers and data types for calculations.

---
+++ day1b.c
@@ -0,0 +1,21 @@
+//Q2: Write a program to input two numbers and display their sum, difference, product, and quotient.
+
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    int  sum, diff, product, quotient;
+    printf("Enter first number: ");
+    scanf("%f", &a);
+    printf("Enter second number: ");
+    scanf("%f", &b);
+    sum = a + b;
+    diff = a - b;
+    product = a * b;
+    quotient = a/b;
+    printf("Sum = %d\n", sum);
+    printf("Difference = %d\n", diff);
+    printf("Product = %d\n", product);
+    printf("Quotient = %d\n", quotient);
+    return 0;
+}
