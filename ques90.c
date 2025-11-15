9f1e1c06623302ae28dbda9a3a001093eda68b6f
maulisingal03 <mauliamb03@gmail.com>
2025-11-15 20:53:36 +0530

    Create ques90.c

---
+++ ques90.c
@@ -0,0 +1,23 @@
+// Q90: Toggle case of each character in a string.
+#include <stdio.h>
+
+int main() {
+    char string[100];
+    printf("Enter a string:");
+    fgets(string, 100, stdin);
+	int i;
+    for (i = 0; string[i] != '\0'; i++) {
+        
+        if (string[i] >= 'a' && string[i] <= 'z') {
+            string[i] -= 32; 
+        } 
+        
+        else if (string[i] >= 'A' && string[i] <= 'Z') {
+            string[i] += 32; 
+        }
+        
+    }
+
+    printf("Modified string:\n%s", string);
+    return 0;
+}
