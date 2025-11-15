704326032b05dfb4da41263e4af0e0b5d8044f9f
maulisingal03 <mauliamb03@gmail.com>
2025-11-15 20:50:20 +0530

    Create ques85.c

---
+++ ques85.c
@@ -0,0 +1,26 @@
+//Q85: Reverse a string.
+
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    fgets(str, sizeof(str), stdin);
+
+    int len = 0;
+
+    while (str[len] != '\0') {
+        len++;
+    }
+    
+    if (str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+        len--;
+    }
+	int i;
+    for (i = len - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
+
