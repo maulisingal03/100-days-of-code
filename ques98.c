953b83c213dc46e4a303054dd6e581f17587b72e
maulisingal03 <mauliamb03@gmail.com>
2025-11-15 21:47:41 +0530

    Create ques98.c

---
+++ ques98.c
@@ -0,0 +1,29 @@
+// Q98: Print initials of a name with the surname displayed in full.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[100];
+    int i, len, ls = -1;
+
+    printf("Enter a name: ");
+    fgets(name, sizeof(name), stdin);
+
+    len = strlen(name);
+
+    for (i = 0; i < len; i++) {
+        if (name[i] == ' ')
+            ls = i;
+    }
+
+    printf("%c.", name[0]);
+
+    for (i = 0; i < ls; i++) {
+        if (name[i] == ' ' && name[i + 1] != ' ')
+            printf("%c.", name[i + 1]);
+    }
+
+    printf(" %s", &name[ls + 1]);
+
+    return 0;
+}
