fb1dbf0310d071abb6fe923159a711ec7f15a6e3
maulisingal03 <mauliamb03@gmail.com>
2025-11-15 20:52:19 +0530

    Create ques88.c

---
+++ ques88.c
@@ -0,0 +1,17 @@
+// Q88: Replace spaces with hyphens in a string.
+
+#include <stdio.h>
+
+int main() {
+    char str[1000];
+    fgets(str, sizeof(str), stdin);  
+    int i;
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ') {
+            str[i] = '-'; 
+        }
+    }
+
+    printf("%s", str);
+    return 0;
+}
